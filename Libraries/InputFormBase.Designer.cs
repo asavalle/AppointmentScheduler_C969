b18643890427ed3d8417af3935a0129a752482ad
Anthony Avalle <anthonyavalle@hotmail.com>
2022-07-22 05:45:22 -0400

    Added InputFormBase - to become a library for Visual Inheritance - need to complete task of compiling to DLL.

---
+++ Libraries/InputFormBase.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace AppointmentScheduler_C969
+{
+    partial class InputFormBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_addAptForm = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.genButton = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lb_endTime = new System.Windows.Forms.Label();
+            this.lb_startTime = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lb_aptDate = new System.Windows.Forms.Label();
+            this.cb_customer = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_aptDesc = new System.Windows.Forms.TextBox();
+            this.tb_aptTitle = new System.Windows.Forms.TextBox();
+            this.tb_aptID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.gb_addAptForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb_addAptForm
+            // 
+            this.gb_addAptForm.BackColor = System.Drawing.Color.AliceBlue;
+            this.gb_addAptForm.Controls.Add(this.label7);
+            this.gb_addAptForm.Controls.Add(this.label6);
+            this.gb_addAptForm.Controls.Add(this.label5);
+            this.gb_addAptForm.Controls.Add(this.label4);
+            this.gb_addAptForm.Controls.Add(this.label3);
+            this.gb_addAptForm.Controls.Add(this.label2);
+            this.gb_addAptForm.Controls.Add(this.textBox3);
+            this.gb_addAptForm.Controls.Add(this.textBox2);
+            this.gb_addAptForm.Controls.Add(this.textBox1);
+            this.gb_addAptForm.Controls.Add(this.button1);
+            this.gb_addAptForm.Controls.Add(this.genButton);
+            this.gb_addAptForm.Controls.Add(this.comboBox2);
+            this.gb_addAptForm.Controls.Add(this.lb_endTime);
+            this.gb_addAptForm.Controls.Add(this.lb_startTime);
+            this.gb_addAptForm.Controls.Add(this.comboBox1);
+            this.gb_addAptForm.Controls.Add(this.dateTimePicker1);
+            this.gb_addAptForm.Controls.Add(this.lb_aptDate);
+            this.gb_addAptForm.Controls.Add(this.cb_customer);
+            this.gb_addAptForm.Controls.Add(this.label1);
+            this.gb_addAptForm.Controls.Add(this.tb_aptDesc);
+            this.gb_addAptForm.Controls.Add(this.tb_aptTitle);
+            this.gb_addAptForm.Controls.Add(this.tb_aptID);
+            this.gb_addAptForm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.gb_addAptForm.Location = new System.Drawing.Point(12, 12);
+            this.gb_addAptForm.Name = "gb_addAptForm";
+            this.gb_addAptForm.Size = new System.Drawing.Size(411, 629);
+            this.gb_addAptForm.TabIndex = 11;
+            this.gb_addAptForm.TabStop = false;
+            this.gb_addAptForm.Text = "Add New Appointment";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(26, 511);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(349, 23);
+            this.textBox3.TabIndex = 23;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 466);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(349, 23);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 23);
+            this.textBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(330, 581);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // genButton
+            // 
+            this.genButton.BackColor = System.Drawing.Color.LightBlue;
+            this.genButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.genButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.genButton.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.genButton.Location = new System.Drawing.Point(229, 581);
+            this.genButton.Name = "genButton";
+            this.genButton.Size = new System.Drawing.Size(84, 37);
+            this.genButton.TabIndex = 17;
+            this.genButton.Text = "Submit";
+            this.genButton.UseVisualStyleBackColor = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(149, 360);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(108, 23);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // lb_endTime
+            // 
+            this.lb_endTime.AutoSize = true;
+            this.lb_endTime.Location = new System.Drawing.Point(149, 344);
+            this.lb_endTime.Name = "lb_endTime";
+            this.lb_endTime.Size = new System.Drawing.Size(56, 15);
+            this.lb_endTime.TabIndex = 15;
+            this.lb_endTime.Text = "End Time";
+            // 
+            // lb_startTime
+            // 
+            this.lb_startTime.AutoSize = true;
+            this.lb_startTime.Location = new System.Drawing.Point(26, 343);
+            this.lb_startTime.Name = "lb_startTime";
+            this.lb_startTime.Size = new System.Drawing.Size(60, 15);
+            this.lb_startTime.TabIndex = 14;
+            this.lb_startTime.Text = "Start Time";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 360);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(108, 23);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(26, 415);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(246, 23);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // lb_aptDate
+            // 
+            this.lb_aptDate.AutoSize = true;
+            this.lb_aptDate.Location = new System.Drawing.Point(26, 398);
+            this.lb_aptDate.Name = "lb_aptDate";
+            this.lb_aptDate.Size = new System.Drawing.Size(58, 15);
+            this.lb_aptDate.TabIndex = 9;
+            this.lb_aptDate.Text = "Start Date";
+            // 
+            // cb_customer
+            // 
+            this.cb_customer.FormattingEnabled = true;
+            this.cb_customer.Location = new System.Drawing.Point(105, 70);
+            this.cb_customer.Name = "cb_customer";
+            this.cb_customer.Size = new System.Drawing.Size(270, 23);
+            this.cb_customer.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Label Text";
+            // 
+            // tb_aptDesc
+            // 
+            this.tb_aptDesc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_aptDesc.Location = new System.Drawing.Point(26, 215);
+            this.tb_aptDesc.MaxLength = 250;
+            this.tb_aptDesc.Multiline = true;
+            this.tb_aptDesc.Name = "tb_aptDesc";
+            this.tb_aptDesc.Size = new System.Drawing.Size(349, 110);
+            this.tb_aptDesc.TabIndex = 2;
+            // 
+            // tb_aptTitle
+            // 
+            this.tb_aptTitle.Location = new System.Drawing.Point(26, 119);
+            this.tb_aptTitle.Name = "tb_aptTitle";
+            this.tb_aptTitle.Size = new System.Drawing.Size(349, 23);
+            this.tb_aptTitle.TabIndex = 1;
+            // 
+            // tb_aptID
+            // 
+            this.tb_aptID.Location = new System.Drawing.Point(26, 70);
+            this.tb_aptID.Name = "tb_aptID";
+            this.tb_aptID.Size = new System.Drawing.Size(51, 23);
+            this.tb_aptID.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Label Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Label Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 15);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Label Text";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 15);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Label Text";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 448);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 15);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Label Text";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 493);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 15);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Label Text";
+            // 
+            // InputFormBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 657);
+            this.Controls.Add(this.gb_addAptForm);
+            this.Name = "InputFormBase";
+            this.Text = "InputFormBase";
+            this.gb_addAptForm.ResumeLayout(false);
+            this.gb_addAptForm.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_addAptForm;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button genButton;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lb_endTime;
+        private System.Windows.Forms.Label lb_startTime;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lb_aptDate;
+        private System.Windows.Forms.ComboBox cb_customer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_aptDesc;
+        private System.Windows.Forms.TextBox tb_aptTitle;
+        private System.Windows.Forms.TextBox tb_aptID;
+    }
+}
---
+++ Libraries/InputFormBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AppointmentScheduler_C969
+{
+    public partial class InputFormBase : Form
+    {
+        public InputFormBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
